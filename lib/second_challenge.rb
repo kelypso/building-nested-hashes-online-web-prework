4bc0f773123b1dc119388f28c9480a19af7161ff
Kelsey Low <kklowlow@gmail.com>
2019-01-08 03:26:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,9 +9,8 @@ def second_challenge
     :capulet => {
       :patriarch => {}
       :matriarch => {}
-      :hero => {}
+      :heroine => {}
       :heroine_friends => []
-      }}
-
-  
+      }
+  }
 end
